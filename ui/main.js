e99263d2e357a608913d91d5b129ddb5a08a89e1
Saravanan S <saravanan-selvaraju@users.noreply.github.com>
2018-02-19 22:05:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,10 @@
 console.log('Loaded!');
 alert('Hi welcome to my site');
+var img=document.getElementById("image");
+function moveRight(){
+    marginleft=marginleft+5;
+    img.style.marginleft=marginleft+"px";
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,50);
+};
