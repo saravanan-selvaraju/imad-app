db381da856dd6b4e63e204bfef31b41733150245
Saravanan S <saravanan-selvaraju@users.noreply.github.com>
2018-02-22 20:58:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,15 +8,27 @@ function moveRight(){
 img.onclick=function(){
     var interval=setInterval(moveRight,50);
 };
-var nameval=document.getElementById('name');
-var name=nameval.value;
 var submit=document.getElementById('submit');
 submit.onclick = function(){
-      var names=['saravanana','ragul','sasi'];
-      var list='';
-      for(var i=0;i<names.length;i++){
-          list=list+'<li>'+names[i]+'</li>';
-      }
-      var ul=document.getElementById('ul');
-      ul.innerHTML=list;
+      var request=new XMLHttpRequest();
+      request.onreadystatechange=function(){
+          if(request.readyState== XMLHttpRequest.DONE){
+            if(request.status==200){
+                  var n=request.responseText;
+                  n=JSON.parse(n)
+                  var list='';
+                  for(var i=0;i<n.length;i++){
+                      list=list+'<li>'+n[i]+'</li>';
+                  }
+                  var ul=document.getElementById('ul');
+                  ul.innerHTML=list;            
+            }      
+          }
+      };
+          
+    var nameval=document.getElementById('name');
+    var names=nameval.value;
+      request.open('GET','http://saravanansdev.imad.hasura-app.io/submit-name?name='+names,true);
+      request.send(null);
+    
 };
