c54ddd6139c9aa9372837b7eeb3bd7fb6dc08d54
Saravanan S <saravanan-selvaraju@users.noreply.github.com>
2018-02-22 20:25:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,3 +8,15 @@ function moveRight(){
 img.onclick=function(){
     var interval=setInterval(moveRight,50);
 };
+var nameval=document.getElemetById("name");
+var name=nameval.value;
+var submit=document.getElementById("submit");
+submit.onclick = function(){
+      var names=['saravanana','ragul','sasi'];
+      var list='';
+      for(var i=0;i<names.length;i++){
+          list=list+"<li>"+names[i]+"</li>";
+      }
+      var ul=document.getElementById("ul");
+      ul.innerHTML=list;
+};
